625bd4ee22d7ea0afb35bd54490a45b3d1c36e51
Savelii Sporyshev <savva.2004.02.27@gmail.com>
2023-05-16 21:40:47 +0300

    feat: исправить изображения для свободного ответа

--- resources/views/admin/quetions/editFreeAnswer.blade.php
+++ resources/views/admin/quetions/editFreeAnswer.blade.php
@@ -9,9 +9,9 @@
 <body>
 <div class="container mt-5">
     <h1>Редактирование теста</h1>
-    <form method="post" action="{{ route('questions.update', $question->id) }}">
+    <form method="post" action="{{ route('questions.update', $question->id) }}" enctype="multipart/form-data">
     @csrf
-    @method('PUT')  
+    @method('PUT')
     <!--<div class="form-group">
         <label for="course_number">Введите номер курса:</label>
         <input type="text" class="form-control" id="course_number" name="course_number" required>
@@ -25,17 +25,21 @@
             @php
                 $data = json_decode($question->data, true);
             @endphp
-            
+
 
             <div class="question form-group">
     <label for="question_1">Вопрос:</label>
     <input type="text" class="form-control" id="question_1" name="questions[1][question]" value="{{$data[0]['question']}}" required>
 
-    
+    <div class="form-group">
+    <label for="image">Выберите изображение:</label>
+    <input type="file" class="form-control-file" id="image" name="image" accept="image/*">
+    </div>
+
     <div class="form-check answer">
         <input type="checkbox" class="form-check-input" name="questions[1][correct_answer][]" value="1" style="pointer-events : none">
         <label class="form-check-label" for="answer_1_1">Ответ 1:</label>
- 
+
         <input type="text" class="form-control" id="answer_1_1" name="questions[1][answers][1]" readonly>
     </div>
 
@@ -93,13 +97,13 @@
 
     <button type="submit" class="btn btn-primary">Сохранить вопрос</button>
 </form>
- 
+
 </div>
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
 <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
- 
+
 
 
 
