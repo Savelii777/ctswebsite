84fc07d57a76e7389964672df300c996978176f8
Savelii Sporyshev <savva.2004.02.27@gmail.com>
2023-10-22 18:38:45 +0300

    статистика

--- app/Http/Controllers/Admin/HomeController.php
+++ app/Http/Controllers/Admin/HomeController.php
@@ -30,10 +30,54 @@ class HomeController extends Controller
     {
         $users_count = User::count();
         $users_attempts = Order::count();
+        $orders_last_month = Order::where('created_at', '>=', now()->subMonth())->count();
+        $orders_last_24_hours = Order::where('created_at', '>=', now()->subDay())->count();
+
+        $all_orders = Order::all();
+
+        // Создаем переменные для хранения сумм
+        $total_all_time = 0;
+        $total_last_month = 0;
+        $total_last_24_hours = 0;
+
+        // Итерируемся по всем заказам
+        foreach ($all_orders as $order) {
+            // Декодируем JSON в массив
+            $order_info = json_decode($order->order_info, true);
+            // Проверяем, существует ли массив и является ли он массивом
+            if (is_array($order_info)) {
+                foreach ($order_info as $item) {
+
+                    // Проверяем, существует ли ключ 'total' в элементе
+                    if (isset($item['total'], $item['quantity'])) {
+                        // Преобразуем значения 'total' и 'quantity' в числа
+                        $total = floatval($item['total']);
+                        $quantity = floatval($item['quantity']);
+                        if (is_numeric($total) && is_numeric($quantity)) {
+                            $total_all_time += $total * $quantity;
+
+                            // Проверяем, когда был создан заказ
+                            $created_at = $order->created_at;
+                            if ($created_at >= now()->subMonth()) {
+                                $total_last_month += $total * $quantity;
+                            }
+                            if ($created_at >= now()->subDay()) {
+                                $total_last_24_hours += $total * $quantity;
+                            }
+                        }
+                    }
+                }
+            }
+        }
 
         return view('admin.home.index', [
             'users_count' => $users_count,
-            'users_attempts' => $users_attempts
+            'users_attempts' => $users_attempts,
+            'orders_last_month' =>  $orders_last_month,
+            'orders_last_24_hours' => $orders_last_24_hours,
+            'total_all_time' => $total_all_time,
+            'total_last_month' => $total_last_month,
+            'total_last_24_hours' => $total_last_24_hours,
         ]);
     }
 }
--- resources/views/admin/home/index.blade.php
+++ resources/views/admin/home/index.blade.php
@@ -41,7 +41,7 @@
               <div class="inner">
                 <h3>{{ $users_attempts }}<sup style="font-size: 20px"></sup></h3>
 
-                <p>Заказы</p>
+                <p>Заказов за все время</p>
               </div>
               <div class="icon">
                 <i class="ion ion-stats-bars"></i>
@@ -49,8 +49,79 @@
               <a href="/admin/tests" class="small-box-footer">Перейти <i class="fas fa-arrow-circle-right"></i></a>
             </div>
           </div>
+            <div class="col-lg-3 col-6">
+                <!-- small box -->
+                <div class="small-box bg-success">
+                    <div class="inner">
+                        <h3>{{ $users_attempts }}<sup style="font-size: 20px"></sup></h3>
+
+                        <p>Заказов за последний месяц</p>
+                    </div>
+                    <div class="icon">
+                        <i class="ion ion-stats-bars"></i>
+                    </div>
+                    <a href="/admin/tests" class="small-box-footer">Перейти <i class="fas fa-arrow-circle-right"></i></a>
+                </div>
+            </div>
+            <div class="col-lg-3 col-6">
+                <!-- small box -->
+                <div class="small-box bg-success">
+                    <div class="inner">
+                        <h3>{{ $orders_last_24_hours }}<sup style="font-size: 20px"></sup></h3>
+
+                        <p>Заказов за последние 24 часа</p>
+                    </div>
+                    <div class="icon">
+                        <i class="ion ion-stats-bars"></i>
+                    </div>
+                    <a href="/admin/tests" class="small-box-footer">Перейти <i class="fas fa-arrow-circle-right"></i></a>
+                </div>
+            </div> <div class="col-lg-3 col-6">
+                <!-- small box -->
+                <div class="small-box bg-success">
+                    <div class="inner">
+                        <h3>{{ $total_all_time}} ₽<sup style="font-size: 20px"></sup></h3>
+
+                        <p>Сумма заказов за все время</p>
+                    </div>
+                    <div class="icon">
+                        <i class="ion ion-stats-bars"></i>
+                    </div>
+                    <a href="/admin/tests" class="small-box-footer">Перейти <i class="fas fa-arrow-circle-right"></i></a>
+                </div>
+            </div>
+            <div class="col-lg-3 col-6">
+                <!-- small box -->
+                <div class="small-box bg-success">
+                    <div class="inner">
+                        <h3>{{$total_last_month}} ₽<sup style="font-size: 20px"></sup></h3>
+
+                        <p>Сумма заказов за последний месяц</p>
+                    </div>
+                    <div class="icon">
+                        <i class="ion ion-stats-bars"></i>
+                    </div>
+                    <a href="/admin/tests" class="small-box-footer">Перейти <i class="fas fa-arrow-circle-right"></i></a>
+                </div>
+            </div>
+            <div class="col-lg-3 col-6">
+                <!-- small box -->
+                <div class="small-box bg-success">
+                    <div class="inner">
+                        <h3>{{ $total_last_24_hours}} ₽<sup style="font-size: 20px"></sup></h3>
+
+                        <p>Сумма заказов за последние 24 часа</p>
+                    </div>
+                    <div class="icon">
+                        <i class="ion ion-stats-bars"></i>
+                    </div>
+                    <a href="/admin/tests" class="small-box-footer">Перейти <i class="fas fa-arrow-circle-right"></i></a>
+                </div>
+            </div>
+
           <!-- ./col -->
         </div>
+
         <!-- /.row -->
       </div><!-- /.container-fluid -->
     </section>
