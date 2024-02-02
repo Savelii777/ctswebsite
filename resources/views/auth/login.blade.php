97b10312afd8f7f1e3f772f4f07af56d67eaf014
Savelii777 <sporyshev.savelii@gmail.com>
2024-02-02 14:38:14 +0300

    fix bug form

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -11,43 +11,44 @@
 @endif
     <div class="entrance__block">
 
-             <form method="POST" action="{{ route('login') }}" class="auth-block">
-                @csrf
-                <p class="form-title">
-                  Вход
-                </p>
-                <div class="row">
-                   <div class="form-group">
-                      <label style="color:#6b7fe3">Введите Ваш логин</label>
-                      <input id="login" type="login" style="color:#000; border: 1px solid #6b7fe3" class="form-control @error('login') is-invalid @enderror" name="login" value="{{ old('login') }}" required autocomplete="login" autofocus>
-                      @error('login')
-                            <span class="invalid-feedback" role="alert">
-                                <strong>{{ $message }}</strong>
-                            </span>
-                        @enderror
-                   </div>
-                </div>
-                <div class="row">
-                   <div class="form-group">
-                      <label style="color:#6b7fe3">Введите Ваш пароль</label>
-                      <input id="password" type="password" style="color:#000; border: 1px solid #6b7fe3" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="current-password">
-                      @error('password')
-                      <span class="invalid-feedback" role="alert">
-                          <strong>{{ $message }}</strong>
-                      </span>
-                      @enderror
-                    </div>
-                </div>
-                <button class="form-btn" type="submit"><a>Войти</a></button>
-               <div class="form-txt" style="display: flex; flex-direction: column">
-                   <a href="/register" style="color: #6b7fe3; max-width: max-content; margin-bottom: 15px">Регистрация</a>
-                   @if (Route::has('password.request'))
-                       <a href="{{ route('password.request') }}" style="color: #6b7fe3; max-width: max-content; ">
-                           Восстановить пароль?
-                       </a>
-                   @endif
-               </div>
-             </form>
+    <form method="POST" action="{{ route('login') }}" class="auth-block">
+    @csrf
+    <p class="form-title">
+        Вход
+    </p>
+    <div class="row">
+        <div class="form-group">
+            <label style="color:#6b7fe3">Введите Ваш логин</label>
+            <input id="login" type="text" style="color:#000; border: 1px solid #6b7fe3" class="form-control @error('login') is-invalid @enderror" name="login" value="{{ old('login') }}" required autocomplete="login" autofocus>
+            @error('login')
+            <span class="invalid-feedback" role="alert">
+                <strong>{{ $message }}</strong>
+            </span>
+            @enderror
+        </div>
+    </div>
+    <div class="row">
+        <div class="form-group">
+            <label style="color:#6b7fe3">Введите Ваш пароль</label>
+            <input id="password" type="password" style="color:#000; border: 1px solid #6b7fe3" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="current-password">
+            @error('password')
+            <span class="invalid-feedback" role="alert">
+                <strong>{{ $message }}</strong>
+            </span>
+            @enderror
+        </div>
+    </div>
+    <button type="submit" class="form-btn">Войти</button>
+    <div class="form-txt" style="display: flex; flex-direction: column">
+        <a href="/register" style="color: #6b7fe3; max-width: max-content; margin-bottom: 15px">Регистрация</a>
+        @if (Route::has('password.request'))
+        <a href="{{ route('password.request') }}" style="color: #6b7fe3; max-width: max-content; ">
+            Восстановить пароль?
+        </a>
+        @endif
+    </div>
+</form>
+
           </div>
     </div>
  </section>
